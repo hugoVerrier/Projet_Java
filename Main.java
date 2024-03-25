168a0c9380c1dc26c53b5f2b11b9dcf7d688f574
eliotrineau <eliot.rineau@edu.ece.fr>
2024-03-25 18:32:03 +0100

    validation

---
+++ Main.java
@@ -0,0 +1,26 @@
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Ma fenêtre basique JavaFX");
+
+        StackPane root = new StackPane();
+        root.getChildren().add(new Text("Bonjour, monde !"));
+
+        Scene scene = new Scene(root, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+        System.out.println("Hugo - Eliot");
+    }
+}
