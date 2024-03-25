eb65f3a860921fe652bd04fa1e55ad90311a7d01
eliotrineau <eliot.rineau@edu.ece.fr>
2024-03-25 16:51:55 +0100

    renvoie

--- Projet/src/Main.java
+++ Projet/src/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        System.out.println("-------------\nEliot Rineau - Hugo Verrier\n-------------\n");
+        System.out.println("-------------\nEliot Rineau -- Hugo Verrier\n-------------\n");
         launch(args);
     }
 }
